f7d237f36c62fb09f4dbf69c8d6940dff09ca3e6
milliwonkim <rldnjs9347@gmail.com>
2021-06-12 18:32:02 +0900

    [feat] add project with file based routing

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FilteredEventsPage() {
+    return (
+        <div>
+            <h1>Filter Events</h1>
+        </div>
+    );
+}
+
+export default FilteredEventsPage;
